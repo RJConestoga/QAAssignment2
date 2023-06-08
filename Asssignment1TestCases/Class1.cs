c0dcc01ba9256455ffe66e559e26f05552538352
RJConestoga <rjasoliya8217@conestogac.on.ca>
2023-06-08 04:19:35 -0400

    Final Commit after 15 test cases

--- Asssignment1TestCases/Class1.cs
+++ Asssignment1TestCases/Class1.cs
@@ -10,7 +10,7 @@ namespace Asssignment1TestCases
 {
     public class Class1
     {
-        /*Req 1*/
+        /*Req 1 - Only one (1) test for a valid equilateral triangle*/
         [Test]
         public void ValidEquilateralTriangleTest_CorrectSideLength_ExpectedTrue()
         {
@@ -23,7 +23,7 @@ namespace Asssignment1TestCases
             Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
         }
 
-        /*Req 2*/
+        /*Req 2 - Three (3) tests for a valid isosceles triangle*/
         [Test]
         public void ValidIsoscelesTriangleTest1_CorrectSideLengths_ExpectedIsosceles()
         {
@@ -60,7 +60,7 @@ namespace Asssignment1TestCases
             Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
         }
 
-        /*Req 3*/
+        /*Req 3 - Five (5) tests for a valid scalene triangle*/
         [Test]
         public void ValidScaleneTriangleTest1_CorrectSideLengths_ExpectedScalene()
         {
@@ -119,6 +119,80 @@ namespace Asssignment1TestCases
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
 
             Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+        }
+
+        /*Req 4 - Three (3) tests for verifying a zero length for one or more sides*/
+        [Test]
+        public void ZeroLengthTest1_ZeroLengthForSide_ExpectedInvalidTriangle()
+        {
+            int side1 = 0;
+            int side2 = 5;
+            int side3 = 6;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", result);
+        }
+
+        [Test]
+        public void ZeroLengthTest2_ZeroLengthForSide_ExpectedInvalidTriangle()
+        {
+            int side1 = 50;
+            int side2 = 0;
+            int side3 = 60;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", result);
+        }
+
+        [Test]
+        public void ZeroLengthTest3_ZeroLengthForSide_ExpectedInvalidTriangle()
+        {
+            int side1 = 72;
+            int side2 = 73;
+            int side3 = 0;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", result);
+        }
+
+        /*Req 5 - Three (3) tests for verifying an invalid response (other than a zero length)*/
+        [Test]
+        public void InvalidOtherThanZeroTest1_InvalidInput_NoTriangleFormed()
+        {
+            int side1 = 10;
+            int side2 = 10;
+            int side3 = 20;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("A triangle cannot be formed with those numbers", result);
+        }
+
+        [Test]
+        public void InvalidOtherThanZeroTest2_InvalidInput_NoTriangleFormed()
+        {
+            int side1 = 2;
+            int side2 = 2;
+            int side3 = 4;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("A triangle cannot be formed with those numbers", result);
+        }
+
+        [Test]
+        public void InvalidOtherThanZeroTest3_InvalidInput_NoTriangleFormed()
+        {
+            int side1 = 10;
+            int side2 = 20;
+            int side3 = 40;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("A triangle cannot be formed with those numbers", result);
         }
 
     }

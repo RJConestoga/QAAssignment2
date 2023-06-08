ee27c99ca6987722901b2f714c8b516dd97b7cde
RJConestoga <rjasoliya8217@conestogac.on.ca>
2023-06-08 03:33:01 -0400

    Commiting first 4 test cases

--- Asssignment1TestCases/Class1.cs
+++ Asssignment1TestCases/Class1.cs
@@ -11,33 +11,52 @@ namespace Asssignment1TestCases
     public class Class1
     {
         [Test]
-        public void ValidEquilateralTriangle()
+        public void ValidEquilateralTriangleTest_CorrectSideLength_ExpectedTrue()
         {
-            // Arrange
             int side1 = 5;
             int side2 = 5;
             int side3 = 5;
 
-            // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
 
-            // Assert
             Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
         }
 
+        
         [Test]
-        public void ValidIsoscelesTriangle1()
+        public void ValidIsoscelesTriangleTest1_CorrectSideLengths_ExpectedIsosceles()
         {
-            // Arrange
             int side1 = 5;
             int side2 = 5;
             int side3 = 8;
 
-            // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
 
-            // Assert
-            Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            Assert.AreEqual("Test Case 1: This triangle is valid and is an ISOSCELES", result);
+        }
+
+        [Test]
+        public void ValidIsoscelesTriangleTest2_CorrectSideLengths_ExpectedIsosceles()
+        {
+            int side1 = 4;
+            int side2 = 7;
+            int side3 = 7;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("Test Case 2: This triangle is valid and is an ISOSCELES", result);
+        }
+
+        [Test]
+        public void ValidIsoscelesTriangleTest3_CorrectSideLengths_ExpectedIsosceles()
+        {
+            int side1 = 4;
+            int side2 = 7;
+            int side3 = 7;
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.AreEqual("Test Case 3: This triangle is valid and is an ISOSCELES", result);
         }
 
     }
